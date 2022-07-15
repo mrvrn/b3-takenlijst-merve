8528fbdf68d2afbe0f42f42cacba5ebe6879ea37
mrvrn <64770873+mrvrn@users.noreply.github.com>
2022-07-15 19:01:44 +0200

    Header aangemaakt
    Ik heb een header aangemaakt en deze toegevoegd aan de pagina's.

---
+++ header.php
@@ -0,0 +1,12 @@
+<header>
+    <div class="nav-bar">
+        <div class="wrapper">
+        <div class="header-menu">
+            <img src="<?php echo $base_url; ?>/img/logo-big-v4.png" class="logo">
+            <nav>
+                <a href="<?php echo $base_url; ?>/tasks/index.php">Home</a>
+            </nav>
+        </div>
+        </div>
+    </div>    
+</header>
---
+++ tasks/index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="nl">
+
+<head>
+    <title></title>
+    <?php require_once '../head.php'; ?>
+</head>
+
+<body>
+
+    <?php require_once '../header.php'; ?>
+
+    <main>
+        <div class="wrapper">
+            <div class="container">
+
+            </div>
+        </div>
+    </main>
+
+</body>
+
+</html>
